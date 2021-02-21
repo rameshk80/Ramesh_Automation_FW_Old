eb3559519d6f5d13c50719c00417afcf045b9a48
Ramesh <rasandesh@Rameshkumars-MacBook-Air.local>
2021-02-21 10:42:52 -0600

    changes in configuration file

--- src/SelWork/WebdriverFactory.java
+++ src/SelWork/WebdriverFactory.java
@@ -9,53 +9,38 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-public class WebdriverFactory {
-    private Configuration config;
-    private Properties properties;
-    private WebDriver driver;
+public class WebdriverFactory extends Main{
 
     public WebdriverFactory() {
-        this.config = new Configuration();
-        this.properties = new Properties();
+        config = new Configuration();
+        config.initializeConfiguration();
     }
 
     public void initializeWebdriver()
     {
-        try
-        {
-            properties.load(new FileInputStream("/Users/rasandesh/Code/Java/src/SelWork/config.properties"));
-        }
-        catch(FileNotFoundException exception)
-        {
-            System.out.println("config.properties file not found");
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-        config.setBrowser_name(properties.getProperty("BROWSER_NAME"));
         if(config.getBrowser_name().equalsIgnoreCase ("Firefox"))
         {
             System.setProperty("webdriver.gecko.driver", properties.getProperty("FIREFOX_DRIVER_PATH"));
-            driver = new FirefoxDriver();
+            this.driver = new FirefoxDriver();
         }
         else if(config.getBrowser_name().equalsIgnoreCase("Chrome"))
         {
             System.setProperty("webdriver.chrome.driver", properties.getProperty("CHROME_DRIVER_PATH"));
-            driver = new ChromeDriver();
+            this.driver = new ChromeDriver();
         }
-
-        //Set the application URL
-        config.setApplication_url(properties.getProperty("URL"));
     }
 
     public void navigateTo()
     {
-        config.getDriver().get(config.getApplication_url());
+        this.driver.get(config.getApplication_url());
+    }
+    public void navigateTo(String url)
+    {
+        this.driver.get(url);
     }
 
     public void quit()
     {
-        config.getDriver().quit();
+        this.driver.quit();
     }
 }
